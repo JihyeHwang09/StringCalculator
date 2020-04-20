3a204a7747577763310f38cf4982e9f91b0dae54
JihyeHwang09 <raye9053@gmail.com>
2020-04-20 20:26:02 +0900

    assertEquals() 메서드를 이용해 기대값과 결과값 확인을 자동화

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,24 +8,24 @@ class CalculatorTest {
     @Test
     void add() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(9, 3));
+        assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     void subtract() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(9, 3));
+        assertEquals(3, cal.subtract(6, 3));
     }
 
     @Test
     void multiply() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(9, 3));
+        assertEquals(18, cal.multiply(6, 3));
     }
 
     @Test
     void divide() {
         Calculator cal = new Calculator();
-        System.out.println(cal.divide(9, 3));
+        assertEquals(2, cal.divide(6, 3));
     }
 }
