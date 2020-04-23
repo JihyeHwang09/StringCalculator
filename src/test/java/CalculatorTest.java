d4bad811d755c15a6a1a871cfe73a963d367aee5
JihyeHwang09 <raye9053@gmail.com>
2020-04-23 11:08:37 +0900

    @Before, @After를 이용한 테스트 코드 중복 제거

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,49 @@
+import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+public class CalculatorTest {
+
+    private Calculator cal;
+
+    @Before
+    public void setup() {
+        cal = new Calculator();
+        System.out.println("before");
+    }
 
     @Test
-    void add() {
+    public void add() {
         Calculator cal = new Calculator();
         assertEquals(9, cal.add(6, 3));
+        System.out.println("add");
     }
 
     @Test
-    void subtract() {
+    public void subtract() {
         Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6, 3));
+        System.out.println("subtract");
     }
 
     @Test
-    void multiply() {
+    public void multiply() {
         Calculator cal = new Calculator();
         assertEquals(18, cal.multiply(6, 3));
+        System.out.println("multiply");
     }
 
     @Test
-    void divide() {
+    public void divide() {
         Calculator cal = new Calculator();
         assertEquals(2, cal.divide(6, 3));
+        System.out.println("divide");
+    }
+
+    @After
+    public void teardown() {
+        System.out.println("teardown");
     }
 }
