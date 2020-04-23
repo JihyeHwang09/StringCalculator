ac4ab52ef05282d77905f52bd7597ceafead57c6
JihyeHwang09 <raye9053@gmail.com>
2020-04-23 16:53:21 +0900

    각 메서드가 한 가지 책임만 가지도록 sum과 toInts로 분리한다.
    문자 배열을 숫자 배열로 변환하는 작업과 숫자 배열의 합을 구하는 작업을 다른 메서드로 분리한다.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,10 +6,29 @@ public class StringCalculator {
         }
 
         String[] values = text.split(",");
+        return sum(toInts(values));
+    }
+
+    private int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private int sum(int[] numbers) {
         int sum = 0;
-        for (String value : values) {
-            sum += Integer.parseInt(value);
+        for (int number : numbers) {
+            sum += number;
         }
         return sum;
     }
 }
+
+/*
+각 메서드가 하나의 책임만 가지고 있는가?
+새로 분리한 sum() 메서드는 어떤가?
+-> 문자 배열을 숫자로 변환하는 작업과
+숫자 배열의 합을 구하는 두 가지 일을 하고 있음을 알 수 있다.
+ */
